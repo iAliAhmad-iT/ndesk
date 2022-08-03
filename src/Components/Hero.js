2774e101ffbbef2dabdde84569c1bcca6eeb3fd3
iAliAhmad-iT <likeualvi7@gmail.com>
2022-08-03 18:41:41 +0500

    new file:   src/Components/Header.js 	new file:   src/Components/Hero.js 	new file:   src/Pages/Home.js

---
+++ src/Components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className="container bg-light">
+            <h4>Ali Ahmad!</h4>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Hero from '../Components/Hero'
+const Home = () => {
+  return (
+    <div className='home'>
+        <Header logo='Ali Ahmad'/>
+        <Hero/>
+    </div>
+  )
+}
+
+export default Home
